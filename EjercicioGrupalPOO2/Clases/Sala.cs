0e68afbe5c6257cf05e2fa2bef80ea85e2513f0c
MikeMarquez <mamarquezp@alumnos.uspg.edu.gt>
2025-08-02 16:41:35 -0600

    :hammer: Clases y estructura

---
+++ EjercicioGrupalPOO2/Clases/Sala.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioGrupalPOO2.Clases
+{
+    public abstract class Sala
+    {
+        public string Nombre { get; set; }
+        public int Capacidad { get; set; }
+        public string Ubicacion { get; set; }
+        public Sala(string nombre, int capacidad, string ubicacion)
+        {
+            Nombre = nombre;
+            Capacidad = capacidad;
+            Ubicacion = ubicacion;
+        }
+        public abstract void Reservar();
+    }
+}
